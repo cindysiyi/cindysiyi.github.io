cead65c4cd2713dd3da8741ce3089bf6fb06abbb
whoami1688 <2572036746@qq.com>
2026-02-20 17:00:23 +0800

    照片

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,17 @@
 export const config = {
   heroImg:
     "https://static-mp-748d976e-45ab-488f-a126-2ec7bc90f502.next.bspapp.com/source/01_20260220144620_11799_38.png",
-  // audio:
-  //   "https://static-mp-748d976e-45ab-488f-a126-2ec7bc90f502.next.bspapp.com/source/record.mp3",
-    audio:''
+  audio:
+    "https://static-mp-748d976e-45ab-488f-a126-2ec7bc90f502.next.bspapp.com/source/record.mp3",
+    // audio:''
+  byte_dance: [
+    'https://static-mp-748d976e-45ab-488f-a126-2ec7bc90f502.next.bspapp.com/source/byte1.png'
+  ],
+  shanghai_gov: [
+    'https://static-mp-748d976e-45ab-488f-a126-2ec7bc90f502.next.bspapp.com/source/shanghai1.jpeg',
+    'https://static-mp-748d976e-45ab-488f-a126-2ec7bc90f502.next.bspapp.com/source/shanghai2.jpeg'
+  ],
+  teaching: [
+    'https://static-mp-748d976e-45ab-488f-a126-2ec7bc90f502.next.bspapp.com/source/teacher1.png'
+  ],
 };
